2b3a762d39ce67d52f37aae191c08af59eb51606
EvertonIkeda <100450932+EvertonIkeda@users.noreply.github.com>
2023-12-01 01:02:40 -0300

    Navbar icon color

--- mobile/lib/screens/appointment.dart
+++ mobile/lib/screens/appointment.dart
@@ -213,19 +213,18 @@ class _AppointmentState extends State<Appointment> {
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: Icon(Icons.home, color: Colors.grey[600]),
             label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.medical_information),
+            icon: Icon(Icons.medical_information, color: Color(0xff344fff)),
             label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.person, color: Colors.grey[600]),
             label: '',
           ),
         ],
-        selectedItemColor: Color(0xff344fff),
         onTap: (index) {
           switch (index) {
             case 0:
